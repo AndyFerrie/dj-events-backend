58e5a14e784e529092ee27810f6cd9ae21b58a62
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-09-29 08:34:27 +0100

    Adds logic to me endpoint

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -1,13 +1,38 @@
-'use strict';
+"use strict";
+const { sanitize, validate } = require("@strapi/utils");
 
 /**
  * event controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::event.event', ({ strapi }) => ({
-    me: async (ctx, next) => {
-        ctx.body = "ok-core";
+module.exports = createCoreController("api::event.event", ({ strapi }) => ({
+  me: async (ctx, next) => {
+    const contentType = strapi.contentType("api::event.event");
+    const user = ctx.state.user;
+
+    console.log(user);
+
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
     }
+
+    const data = await strapi.entityService.findMany(contentType.uid, {
+      populate: "*",
+      filters: {
+        user: user.id,
+      },
+    });
+
+    if (!data) {
+      return ctx.notFound();
+    }
+
+    return await sanitize.contentAPI.output(data, contentType, {
+      auth: ctx.state.auth,
+    });
+  },
 }));
